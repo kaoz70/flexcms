7d7c7161c41bdac1b397780495db1583624aa022
Miguel Suarez <miguelsuarez70@gmail.com>
2015-07-09 18:36:39 -0500

    Updated code

--- framework/application/vendor/mrclay/minify/min_extras/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// using same lib path and cache path specified in /min/config.php
-
-require dirname(__FILE__) . '/../min/config.php';
-
-$minifyCachePath = isset($min_cachePath) 
-    ? $min_cachePath 
-    : '';
