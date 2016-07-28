baf92811df36e918c0078db1c4ce7923ca7f4bd6
Miguel Suarez <miguelsuarez70@gmail.com>
2016-07-28 18:04:07 -0500

    Removed some unused files

--- framework/application/modules/auth/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-
-
-/* End of file config.php */
-/* Location: ./framework/application/modules/auth/config/config.php */
