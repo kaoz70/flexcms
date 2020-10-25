f317ea8984a85f1124322c4840e1e839704ac40b
Miguel Suarez <miguel@suarez.pro.ec>
2020-10-24 21:18:30 -0500

    Format fixes, fixed responsive images pixelating

--- flexcms/modules/content/js/Content.js
+++ flexcms/modules/content/js/Content.js
@@ -7,34 +7,28 @@
  *
  * */
 angular.module('app')
-    .factory('Content', function($resource){
-        return $resource(system.base_url + 'admin/content/:method/:id', { id: '@id'}, {
-            query: {
-                isArray: false
-            },
-            save: {
-                method: 'POST'
-            },
-            update: {
-                method: 'PUT'
-            },
-            get: {
-                method: 'GET'
-            },
-            delete: {
-                method: 'DELETE'
-            }
-        });
-    })
-    .factory('ContentConfig', function($resource){
-        return $resource(system.base_url + 'admin/content/config/:id', { id: '@id'}, {
-            update: {
-                method: 'PUT'
-            },
-            get: {
-                method: 'GET'
-            }
-        });
-    })
-
-    ;
+    .factory('Content', ($resource) => $resource(`${system.base_url}admin/content/:method/:id`, { id: '@id' }, {
+        query: {
+            isArray: false,
+        },
+        save: {
+            method: 'POST',
+        },
+        update: {
+            method: 'PUT',
+        },
+        get: {
+            method: 'GET',
+        },
+        delete: {
+            method: 'DELETE',
+        },
+    }))
+    .factory('ContentConfig', ($resource) => $resource(`${system.base_url}admin/content/config/:id`, { id: '@id' }, {
+        update: {
+            method: 'PUT',
+        },
+        get: {
+            method: 'GET',
+        },
+    }));
